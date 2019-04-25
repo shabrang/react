7a9e4b48a5ea8c4de2399551da2478e9cea805d9
Amirhossein Pooladvand <sepidehshabrang70@gmail.com>
2019-04-25 15:38:02 +0430

    Make counter component

--- src/App.js
+++ src/App.js
@@ -1,30 +1,12 @@
-import React, { Component } from 'react'
 import './scss/_init.scss'
+import React, { Component } from 'react'
+import Counter from './components/Counter/Counter'
 
 export default class App extends Component {
-  state = {
-    name: null,
-    value: null
-  }
-
-  showName = () => {
-    this.setState({ name: this.state.value })
-  }
-
-  setName = (event) => {
-    const value = event.target.value
-
-    this.setState({ value: value })
-  }
 
   render () {
-    console.log('Rendering')
     return (
-      <div className='container-fluid mt-3'>
-        <input type="text" onKeyUp={ this.setName }/>
-        <button onClick={ this.showName }>Change Name</button>
-        <span className='ml-2 d-inline-block'>{ this.state.value }</span>
-      </div>
+      <Counter/>
     )
   }
 
